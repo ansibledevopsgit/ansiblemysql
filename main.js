abb6a3a1918ddfe589a54d0bb6f37619d73e367d
@mohamad66 <rahimi@homacall.com>
2024-03-13 23:04:25 +0330

    cop

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ const port = 5000;
   
 //CORS middleware
 var corsMiddleware = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5000'); //replace localhost with actual host
+    res.header('Access-Control-Allow-Origin', 'localhost:5000'); 
+   // res.header('Access-Control-Allow-Origin', 'localhost');//replace localhost with actual host
     res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
 
