a2faf876e920c4ada28fef4480023020583c4330
@mohamad66 <rahimi@homacall.com>
2024-03-13 22:49:21 +0330

    po

--- main.js
+++ main.js
@@ -13,25 +13,16 @@ const app = express();
 const port = 5000;
 
   
-app.use(function (req, res, next) {
+//CORS middleware
+var corsMiddleware = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5000'); //replace localhost with actual host
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
     next();
-});
-  
+}
+
+app.use(corsMiddleware);
  
 
 app.use(bodyParser.urlencoded({ extended: false }));
